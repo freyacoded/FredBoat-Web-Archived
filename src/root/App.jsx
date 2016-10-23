94040d7e4949137f1df0edc840caea0f2fef8ec0
Frederikam <frogkr@gmail.com>
2016-10-23 16:10:15 +0200

    Polished some things

--- src/root/App.jsx
+++ src/root/App.jsx
@@ -12,7 +12,7 @@ class App extends Component {
 						<div className="title">FredBoat</div>
 					</section>
 					<section className="logoSection">
-						<img className="logo" src={logo} style={{display: "none"}} alt="logo"></img>
+						<img className="logo" src={logo} style={{display: "none"}} alt="logo" draggable="false"></img>
 					</section>
 					<section className="page" id="page">
 					</section>
