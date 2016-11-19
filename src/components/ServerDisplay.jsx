24faf2ee6c4661f97b38bf32ed8d83e572f1aea3
Frederikam <frogkr@gmail.com>
2016-11-19 23:44:53 +0100

    Removed old code

--- src/components/ServerDisplay.jsx
+++ src/components/ServerDisplay.jsx
@@ -9,12 +9,6 @@ class ServerDisplay extends Component {
 
 	onHamburgerClick(button) {
 		ServerList.instance.setOpen(!button.state.toggle);
-		//ReactDOM.render(<ServerList isOpen={button.state.toggle}/>,
-		//	document.getElementById("serverListContainer"));
-		/*ReactDOM.render(
-		     <ServerList isOpen={button.state.toggle}/>,
-		     document.getElementById('serverListContainer')
-		);*/
 
 	}
 
@@ -26,11 +20,11 @@ class ServerDisplay extends Component {
 				<div id="ServerDisplay" className="ServerDisplay">
 					<div className="serverIcon" style={style}></div>
 					<div className="serverName">{this.props.guild.name}</div>
-					<SvgToggleButton
+					<!--<SvgToggleButton
 						width="32px"
 						height="32px"
 						type="hamburger" onClick={this.onHamburgerClick}
-						id={"button-" + Math.random().toString(36).substring(12)}/>
+						id={"button-" + Math.random().toString(36).substring(12)}/>-->
 				</div>
 			</div>
 		);
