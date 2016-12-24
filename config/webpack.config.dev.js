699c5f1eddabfb0e7d088147462fc1db105be400
Frederikam <frogkr@gmail.com>
2016-12-25 00:30:38 +0100

    Added typescript loader. Hopefully

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -109,6 +109,11 @@ module.exports = {
           })
         }
       },
+      {
+        test: /\.(ts|tsx)$/,
+        include: paths.appSrc,
+        loader: 'ts-loader'
+      },
       // "postcss" loader applies autoprefixer to our CSS.
       // "css" loader resolves paths in CSS and adds assets as dependencies.
       // "style" loader turns CSS into JS modules that inject <style> tags.
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -104,6 +104,11 @@ module.exports = {
         loader: 'babel',
         
       },
+      {
+        test: /\.(ts|tsx)$/,
+        include: paths.appSrc,
+        loader: 'ts-loader'
+      },
       // The notation here is somewhat confusing.
       // "postcss" loader applies autoprefixer to our CSS.
       // "css" loader resolves paths in CSS and adds assets as dependencies.
